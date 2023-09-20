93ceb38809bc67ef549e114f2b64a54341b640d5
Ricardux <ricardas@ricardas.lt>
2023-09-20 10:29:29 +0300

    Aritmetika ir nodemon install

--- kintamieji/number.js
+++ kintamieji/number.js
@@ -14,6 +14,25 @@ var minusas = -7.599;
 
 const x1 = 1;
 const x6 = 9;
-x7 = 6;
-
+x7 = 3;
+//var = 6;
 console.log(x6);
+console.log(x7);
+console.log(amzius);
+const n1 = 7;
+const n2 = 5;
+const sum = n1 + n2;
+const diff = n1 - n2;
+const multi = n1 * n2;
+const div = n1 / n2;
+
+console.log(sum);   // suma
+console.log(diff);  // skirtumas
+console.log(multi); // daugyba
+console.log(div);   // dalyba
+
+const complex = 2 + 2 * 2; 
+console.log(complex);
+const exp1 = x6*x7;
+const exp2 = x6**x7;
+console.log ( "Sudauginom:", exp1, "     Pakelem laipsniu:", exp2);
