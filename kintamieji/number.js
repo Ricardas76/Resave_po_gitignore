8102cb993b84686a52578ece50172ea5cd4aaaca
Ricardux <ricardas@ricardas.lt>
2023-09-20 11:17:39 +0300

    3diena_

--- kintamieji/number.js
+++ kintamieji/number.js
@@ -8,6 +8,8 @@ number
 - let (jei kinta informacija)
 - var (niekada nenaudoti)
 */
+
+
 const amzius = 99;
 let pinigine = 0;
 var minusas = -7.599;
@@ -36,3 +38,9 @@ console.log(complex);
 const exp1 = x6*x7;
 const exp2 = x6**x7;
 console.log ( "Sudauginom:", exp1, "     Pakelem laipsniu:", exp2);
+
+const txt1 = "tekstas1";
+const txt2 = 'tekstas2';
+
+console.log (txt1);
+console.log (txt2);
